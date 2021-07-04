3e22bff719f28cf4dfcae9a37bbcf893aa7a62ab
TomatoSoup <jonathantang0126@gmail.com>
2021-07-04 15:13:42 +0800

    adjust deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -4,7 +4,7 @@
 set -e
 
 # build
-npm run build
+yarn build
 
 # navigate into the build output directory
 cd dist
@@ -15,11 +15,5 @@ cd dist
 git init
 git add -A
 git commit -m 'deploy'
-
-# if you are deploying to https://<USERNAME>.github.io
-# git push -f git@github.com:<USERNAME>/<USERNAME>.github.io.git master
-
-# if you are deploying to https://<USERNAME>.github.io/<REPO>
-# git push -f git@github.com:<USERNAME>/<REPO>.git master:gh-pages
 
 cd -
